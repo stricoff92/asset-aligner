3a230179b629721cbb8a51121f1b9b421ccca06b
Jon Stricoff <stricoff92@gmail.com>
2024-05-02 06:46:39 -0400

    wip

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,30 @@
 
 console.log("Hello from index.js!")
 
+const canvasId = "display-canvas";
+const containerId = "page-container";
+const DISPLAY_FORMAT_HORIZONTAL = "horizontal";
+const DISPLAY_FORMAT_VERTICAL = "vertical";
+
+function addHTMLElements() {
+
+    const windowW = window.innerWidth;
+    const windowH = window.innerHeight;
+    const canvas = document.createElement("canvas");
+    canvas.id = canvasId;
+    const controlPanel = document.createElement("div");
+
+
+    const displayFormat = windowW > windowH ? DISPLAY_FORMAT_HORIZONTAL : DISPLAY_FORMAT_VERTICAL;
+    console.log({ displayFormat });
+    const pageContainer = document.getElementById(containerId);
+    pageContainer.style.display = "flex";
+
+}
+
+
+
+function main() {
+    addHTMLElements();
+}
+document.addEventListener("DOMContentLoaded", main);
---
+++ serve.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 -m http.server --bind 127.0.0.1 --directory public/ 9000
